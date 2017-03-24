3d048704de92abf34dc835426f51b4b8078d0790
adrianbj <adrianbjones@gmail.com>
2017-03-24 15:54:06 -0700

    Simplify @gmclelland tweaks to cleanup and fix broken Check All option for cleanup list.

--- ProcessModuleToolkit.js
+++ ProcessModuleToolkit.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     });
 
 
-    /* Selected Modules */
+    /* Selected Export Modules */
     $("#wrap_Inputfield_selectedModules #Inputfield_selectedModules_toggleAll").prop("checked", true);
 
     $("#wrap_Inputfield_selectedModules #Inputfield_selectedModules_toggleAll").click(function () {
@@ -39,6 +39,27 @@ $(document).ready(function() {
 
     $("#wrap_Inputfield_selectedModules input[type=checkbox]:not('#Inputfield_selectedModules_toggleAll')").click(function () {
         $("#wrap_Inputfield_selectedModules #Inputfield_selectedModules_toggleAll").prop("checked", false);
+    });
+
+
+    /* Selected Cleanup Modules */
+    $("#wrap_Inputfield_upgradeCleanup #Inputfield_upgradeCleanup_toggleAll").prop("checked", true);
+
+    $("#wrap_Inputfield_upgradeCleanup #Inputfield_upgradeCleanup_toggleAll").click(function () {
+        if ($("#wrap_Inputfield_upgradeCleanup #Inputfield_upgradeCleanup_toggleAll").is(':checked')) {
+            $("#wrap_Inputfield_upgradeCleanup input[type=checkbox]").each(function () {
+                $(this).prop("checked", true);
+            });
+
+        } else {
+            $("#wrap_Inputfield_upgradeCleanup input[type=checkbox]").each(function () {
+                $(this).prop("checked", false);
+            });
+        }
+    });
+
+    $("#wrap_Inputfield_upgradeCleanup input[type=checkbox]:not('#Inputfield_upgradeCleanup_toggleAll')").click(function () {
+        $("#wrap_Inputfield_upgradeCleanup #Inputfield_upgradeCleanup_toggleAll").prop("checked", false);
     });
 
 
