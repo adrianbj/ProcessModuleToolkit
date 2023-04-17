89501820b41e5caebb0005c3c5c033aafb3aa2f1
Adrian Jones <adrianbjones@gmail.com>
2023-04-17 09:08:03 -0700

    jQuery deprecation fixes. Hide unnecessary Toggle All checkbox.
    https://github.com/adrianbj/ProcessModuleToolkit/issues/5
    https://github.com/adrianbj/ProcessModuleToolkit/issues/6

--- ProcessModuleToolkit.js
+++ ProcessModuleToolkit.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     /* Selected Cleanup */
     $("#wrap_Inputfield_upgrade_cleanup #Inputfield_upgrade_cleanup_toggleAll").prop("checked", true);
 
-    $("#wrap_Inputfield_upgrade_cleanup #Inputfield_upgrade_cleanup_toggleAll").click(function () {
+    $("#wrap_Inputfield_upgrade_cleanup #Inputfield_upgrade_cleanup_toggleAll").on('click', function () {
         if ($("#wrap_Inputfield_upgrade_cleanup #Inputfield_upgrade_cleanup_toggleAll").is(':checked')) {
             $("#wrap_Inputfield_upgrade_cleanup input[type=checkbox]").each(function () {
                 $(this).prop("checked", true);
@@ -16,7 +16,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#wrap_Inputfield_upgrade_cleanup input[type=checkbox]:not('#Inputfield_upgrade_cleanup_toggleAll')").click(function () {
+    $("#wrap_Inputfield_upgrade_cleanup input[type=checkbox]:not('#Inputfield_upgrade_cleanup_toggleAll')").on('click', function () {
         $("#wrap_Inputfield_upgrade_cleanup #Inputfield_upgrade_cleanup_toggleAll").prop("checked", false);
     });
 
@@ -24,7 +24,7 @@ $(document).ready(function() {
     /* Selected Export Modules */
     $("#wrap_Inputfield_selectedModules #Inputfield_selectedModules_toggleAll").prop("checked", true);
 
-    $("#wrap_Inputfield_selectedModules #Inputfield_selectedModules_toggleAll").click(function () {
+    $("#wrap_Inputfield_selectedModules #Inputfield_selectedModules_toggleAll").on('click', function () {
         if ($("#wrap_Inputfield_selectedModules #Inputfield_selectedModules_toggleAll").is(':checked')) {
             $("#wrap_Inputfield_selectedModules input[type=checkbox]").each(function () {
                 $(this).prop("checked", true);
@@ -37,7 +37,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#wrap_Inputfield_selectedModules input[type=checkbox]:not('#Inputfield_selectedModules_toggleAll')").click(function () {
+    $("#wrap_Inputfield_selectedModules input[type=checkbox]:not('#Inputfield_selectedModules_toggleAll')").on('click', function () {
         $("#wrap_Inputfield_selectedModules #Inputfield_selectedModules_toggleAll").prop("checked", false);
     });
 
@@ -45,7 +45,7 @@ $(document).ready(function() {
     /* Selected Cleanup Modules */
     $("#wrap_Inputfield_upgradeCleanup #Inputfield_upgradeCleanup_toggleAll").prop("checked", true);
 
-    $("#wrap_Inputfield_upgradeCleanup #Inputfield_upgradeCleanup_toggleAll").click(function () {
+    $("#wrap_Inputfield_upgradeCleanup #Inputfield_upgradeCleanup_toggleAll").on('click', function () {
         if ($("#wrap_Inputfield_upgradeCleanup #Inputfield_upgradeCleanup_toggleAll").is(':checked')) {
             $("#wrap_Inputfield_upgradeCleanup input[type=checkbox]").each(function () {
                 $(this).prop("checked", true);
@@ -58,7 +58,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#wrap_Inputfield_upgradeCleanup input[type=checkbox]:not('#Inputfield_upgradeCleanup_toggleAll')").click(function () {
+    $("#wrap_Inputfield_upgradeCleanup input[type=checkbox]:not('#Inputfield_upgradeCleanup_toggleAll')").on('click', function () {
         $("#wrap_Inputfield_upgradeCleanup #Inputfield_upgradeCleanup_toggleAll").prop("checked", false);
     });
 
